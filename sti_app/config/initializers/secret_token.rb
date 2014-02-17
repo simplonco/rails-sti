878d5a44c41a98fc4a1475ec7e6f987df64191ea
lramapathiran <lramapathiran@gmail.com>
2014-02-17 13:47:37 +0100

    Initial commit

---
+++ sti_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StiApp::Application.config.secret_key_base = '11b33747f16ccb39e07f0a92b17bcd9c22e8f5169e84e4e7ace020456248f13d42c0ba9042f8cfa48cd1f0397e56918897aeba5e9b39d9393c9d5c0efe0d50ae'
